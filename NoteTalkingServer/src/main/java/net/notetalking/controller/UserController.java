bcd25bbc4cfe8faf7d22e840e699608aa4d62f93
Ba Thanh <thanhnb1101@gmail.com>
2023-09-15 14:17:13 +0700

    update

--- NoteTalkingServer/src/main/java/net/notetalking/controller/UserController.java
+++ NoteTalkingServer/src/main/java/net/notetalking/controller/UserController.java
@@ -1,14 +1,20 @@
 package net.notetalking.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import net.notetalking.model.LoginObj;
 import net.notetalking.model.User;
 import net.notetalking.service.LoginService;
 import net.notetalking.service.UserService;
@@ -27,8 +33,20 @@ public class UserController extends BaseController{
 		return new ResponseEntity<User>(response, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ResponseEntity login(@RequestBody LoginObj request) throws Exception {
-		return response(loginService.login(request.getUserName(), request.getPwd()));
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ResponseEntity login(@RequestParam(required = true) String userName, @RequestParam(required = true) String password) throws Exception {
+		return response(loginService.login(userName, password));
+	}
+	
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public boolean logout(HttpServletRequest request, HttpServletResponse response) {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication != null) {
+			new SecurityContextLogoutHandler().logout(request, response, authentication);
+			return true;
+		}
+		return false;
 	}
 }
